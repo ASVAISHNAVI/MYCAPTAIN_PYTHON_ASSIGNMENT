4faaf03d39d8235de174cacdac48140062e3f9d9
ASVAISHNAVI <124903280+ASVAISHNAVI@users.noreply.github.com>
2023-02-18 18:27:25 +0530

    Add files via upload

---
+++ positive num mycaptain.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+
+list1 = [12,-7,5,64,-14]
+num = 0
+while(num < len(list1)):
+    if list1[num]>=0:
+        print(list1[num],end=" ")
+    num+=1
+
+
+# In[ ]:
+
+
+
+
