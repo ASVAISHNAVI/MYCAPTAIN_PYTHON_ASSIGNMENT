acf1ac5d1b5512da570d8dca576c551a628e6100
ASVAISHNAVI <124903280+ASVAISHNAVI@users.noreply.github.com>
2023-02-18 18:31:39 +0530

    Add files via upload

---
+++ letter frequency mycaptain.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+
+from collections import Counter
+test_str = "mississippi"
+res = Counter(test_str)
+print("Count of all characters in mississippi is :\n ")
+print(res)
+
+
+# In[ ]:
+
+
+
+
